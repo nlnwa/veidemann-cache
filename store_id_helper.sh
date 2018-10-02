a52a974a47fd6feb46e632117680b890c8a8c4d3
johnerikhalse <johnerikhalse@gmail.com>
2018-10-02 10:56:02 +0200

    Fixed cache-manager not responding Cleaned up init

--- store_id_helper.sh
+++ store_id_helper.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 echo "Store ID helper $*" >&2
 while read s; do
-  STORE_ID="$(echo ${s} | cut -d' ' -f2)$(echo ${s} | cut -d' ' -f1)"
+  PROTO="$(echo ${s} | cut -d':' -f1)"
+  if [ ${PROTO} = "cache_object" ]; then
+    STORE_ID=$(echo ${s} | cut -d' ' -f1)
+  else
+    STORE_ID="$(echo ${s} | cut -d' ' -f2)$(echo ${s} | cut -d' ' -f1)"
+  fi
   echo "STORE ID: ${STORE_ID}" >&2
   echo "OK store-id=\"${STORE_ID}\""
 done
