56f996a204b950b98a5443128354d8ef70555191
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2021-10-29 09:38:49 +0200

    Refactor confighandler to ensure config is initialized before daemonizing process

--- go/cmd/confighandler/main.go
+++ go/cmd/confighandler/main.go
@@ -15,40 +15,55 @@ import (
 )
 
 func main() {
+	log.SetPrefix("[ConfigHandler] ")
+
 	r := new(rewriter)
 
-	flag.BoolVar(&r.balancer, "b", false, "Set to true to make this node a non-caching load balancing node")
+	flag.BoolVar(&r.balancer, "b", false, "Set to true to configure squid as balancer")
 	flag.Parse()
 
+	// configure rewriter
+	r.configPath = "/etc/squid/squid.conf"
 	if r.balancer {
-		log.Print("ConfigHandler: initialize squid as balancer")
+		log.Print("Configuring squid as balancer")
+		r.templatePath = "/etc/squid/squid-balancer.conf.template"
 		r.discovery = discovery.NewDiscovery()
 	} else {
-		log.Print("ConfigHandler: initialize squid as cache")
+		log.Print("Configuring squid as cache")
+		r.templatePath = "/etc/squid/squid.conf.template"
 	}
 
+	// initial config rewrite
+	if err := r.rewriteConfig(); err != nil {
+		log.Fatalf("Failed to initialize configuration: %v", err)
+	}
+
+	// fork daemon process
 	context := &daemon.Context{LogFileName: "/proc/self/fd/2"}
 	child, err := context.Reborn()
 	if err != nil {
-		log.Fatalf("ConfigHandler: unable to run: %v", err)
+		log.Fatalf("Failed to create daemon process: %v", err)
 	}
-
 	if child != nil {
 		// This code is run in parent process
-		log.Print("ConfigHandler: init done")
-		return
+		log.Printf("Configuration initialized: %s", r.configPath)
 	} else {
 		// This code is run in forked child
-		log.Print("ConfigHandler: daemon started")
+		log.Println("Daemon started")
 		defer func() {
 			_ = context.Release()
-			log.Print("ConfigHandler: daemon stopped")
+			log.Println("Daemon stopped")
 		}()
 		for {
-			if err := r.check(); err != nil {
-				log.Printf("ConfigHandler: %v", err)
-			}
 			time.Sleep(5 * time.Second)
+			if err := r.rewriteConfig(); err != nil {
+				log.Printf("Failed to rewrite configuration: %v", err)
+			}
+			if r.changes {
+				if err := reconfigureSquid(); err != nil {
+					log.Printf("Error reloading squid configuration: %v", err)
+				}
+			}
 		}
 	}
 }
@@ -58,22 +73,46 @@ type rewriter struct {
 	lastDnsServers string
 	discovery      *discovery.Discovery
 	balancer       bool
+	templatePath   string
+	configPath     string
+	changes        bool
 }
 
-func (r *rewriter) check() error {
+func (r *rewriter) rewriteConfig() error {
+	r.changes = false
 	parents := ""
 	if r.balancer {
 		var err error
-		parents, err = r.getPeers()
+		parents, err = r.getParents()
 		if err != nil {
 			return fmt.Errorf("failed to get parents: %w", err)
 		}
+		if parents == "" {
+			return fmt.Errorf("found no parents")
+		}
 	}
 	dnsServers := r.getDnsServersString()
+	if dnsServers == "" {
+		return fmt.Errorf("no dns servers configured")
+	}
 
 	if parents != r.lastParents || dnsServers != r.lastDnsServers {
-		conf := r.rewriteConfig(dnsServers, parents)
-		r.writeConfig(conf)
+		// read template
+		b, err := ioutil.ReadFile(r.templatePath)
+		if err != nil {
+			return fmt.Errorf("failed to read template (%s): %w", r.templatePath, err)
+		}
+		// substitute template variables
+		conf := string(b)
+		conf = strings.Replace(conf, "${DNS_IP}", dnsServers, 1)
+		if r.balancer {
+			conf = strings.Replace(conf, "${PARENTS}", parents, 1)
+		}
+		// write config
+		if err := ioutil.WriteFile(r.configPath, []byte(conf), 777); err != nil {
+			return fmt.Errorf("failed to write config (%s): %w", r.configPath, err)
+		}
+		r.changes = true
 	}
 
 	r.lastParents = parents
@@ -81,14 +120,14 @@ func (r *rewriter) check() error {
 	return nil
 }
 
-func (r *rewriter) getPeers() (string, error) {
-	children, err := r.discovery.GetPeers()
+func (r *rewriter) getParents() (string, error) {
+	parents, err := r.discovery.GetParents()
 	if err != nil {
 		return "", err
 	}
 	var peers string
-	for _, child := range children {
-		peers += fmt.Sprintf("cache_peer %v parent 3128 0 carp no-digest\n", child)
+	for _, parent := range parents {
+		peers += fmt.Sprintf("cache_peer %v parent 3128 0 carp no-digest\n", parent)
 	}
 	return peers, nil
 }
@@ -108,59 +147,38 @@ func (r *rewriter) getDnsServersString() string {
 	return dnsServers
 }
 
-func (r *rewriter) rewriteConfig(dnsServers string, parents string) string {
-	var templatePath string
-	if r.balancer {
-		templatePath = "/etc/squid/squid-balancer.conf.template"
-	} else {
-		templatePath = "/etc/squid/squid.conf.template"
-	}
-	b, err := ioutil.ReadFile(templatePath)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	conf := string(b)
-	conf = strings.Replace(conf, "${DNS_IP}", dnsServers, 1)
-	conf = strings.Replace(conf, "${PARENTS}", parents, 1)
-
-	return conf
-}
-
-func (r *rewriter) writeConfig(conf string) {
-	err := ioutil.WriteFile("/etc/squid/squid.conf", []byte(conf), 777)
-	if err != nil {
-		log.Print(err)
-	}
-
-	p, err := ioutil.ReadFile("/var/run/squid/squid.pid")
-	if err != nil {
-		log.Printf("ConfigHandler: No squid is running %v", err.Error())
-		return
-	}
-
-	log.Printf("ConfigHandler: Reloading squid config...")
-	cmd := exec.Command("kill", "-HUP", strings.Trim(string(p), " \n\r\t"))
+func reconfigureSquid() error {
+	log.Printf("Reconfiguring squid...")
+	cmd := exec.Command("squid", "-k", "reconfigure")
+	// ignore error returned if wait was already called
+	defer func() { _ = cmd.Wait() }()
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to pipe stderr [%s]: %w", cmd.String(), err)
 	}
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to pipe stdout [%s]: %w", cmd.String(), err)
 	}
 
 	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to start [%s]", cmd.String())
 	}
 
-	slurp, _ := ioutil.ReadAll(stdout)
-	log.Printf("%s\n", slurp)
-	slurp, _ = ioutil.ReadAll(stderr)
-	log.Printf("%s\n", slurp)
+	if slurp, err := ioutil.ReadAll(stdout); err != nil {
+		return fmt.Errorf("failed to read standard out [%s]", cmd.String())
+	} else {
+		log.Print(slurp)
+	}
+	if slurp, err := ioutil.ReadAll(stderr); err != nil {
+		return fmt.Errorf("failed to read standard err [%s]", cmd.String())
+	} else {
+		log.Print(slurp)
+	}
 
 	if err := cmd.Wait(); err != nil {
-		log.Printf("ConfigHandler: Reloading squid config finished with error: %v", err)
+		return err
 	}
+	return nil
 }
