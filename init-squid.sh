bf656ef2adea957ed0aea1f986c92fd9812077b6
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2021-10-29 10:56:12 +0200

    Revert environment substition in init-squid.sh

--- init-squid.sh
+++ init-squid.sh
@@ -5,7 +5,8 @@
 # ----------------------------------------------------------------------------
 set -e
 
+# Create and initialize TLS certificates cache directory
 /usr/lib/squid/security_file_certgen -c -s /var/lib/ssl_db -M 4MB
-chown -R squid:squid /var/lib/ssl_db
 
-envsubst '$CACHE_DIR_MB' < /etc/squid/squid.conf > /etc/squid/squid.conf.template
+# Set permissions to allow access by squid
+chown -R squid:squid /var/lib/ssl_db
